a3a6eaf29ceacbe11261c77f005f07c1a65dfe69
tunaxrcher <mystic.outriderxpaine@gmail.com>
2025-10-22 15:43:17 +0700

    .

--- src/app/(protected)/store-admin/inventory/all-products/content.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { AllPlanning } from '@src/app/(protected)/store-admin/inventory/all-products/components/all-planning';
-
-export function AllProductsContent() {
-  return <AllPlanning />;
-}
--- src/app/(protected)/store-client/order-receipt/content.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { OrderReceipt } from '@src/app/(protected)/store-client/order-receipt/components/order-receipt';
-
-export function OrderReceiptContent() {
-  return <OrderReceipt />;
-}
